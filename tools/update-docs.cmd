06baa3714a79028b9d2ec74a1bb2f2e42b8cbd73
Tomas Petricek <tomas@tomasp.net>
2013-06-03 19:44:54 +0100

    update script

--- tools/update-docs.cmd
+++ tools/update-docs.cmd
@@ -5,8 +5,10 @@ REM  - Make sure you do not have any pending changes in `git status`
 REM  - Go to the 'tools' directory and run `./update-docs.sh`
 
 fsi build.fsx
+cd ..
 git checkout gh-pages
-cp ../docs/*.html ../
-git commit -a -m "Update generated documentation"
-git push
+copy /y docs\*.html .
+git add *.html
+git commit -m "Update generated documentation"
+git push origin gh-pages
 git checkout master

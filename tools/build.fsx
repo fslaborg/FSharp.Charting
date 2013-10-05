6fc5ae186e227c6f868d0e21f5e843a7fdbf412e
tpetricek <tomas@tomasp.net>
2013-10-04 21:19:56 -0400

    Improve web site build, add new css

--- tools/build.fsx
+++ tools/build.fsx
@@ -17,11 +17,19 @@ let output   = __SOURCE_DIRECTORY__ ++ "../docs"
 let root = "http://fsharp.github.com/FSharp.Charting"
 
 // When running locally, you can use your path
-//let root = @"file://C:\Tomas\Projects\FSharp.Data\docs"
+//let root = @"file://C:\Tomas\Projects\FSharp.Charting\docs"
 
 
 // Generate HTML from all FSX files in samples & subdirectories
 let build () =
+  // Copy all sample data files to the "data" directory
+  let copy = [ sources ++ "../tools/content", output ++ "content" ]
+  for source, target in copy do
+    if Directory.Exists target then Directory.Delete(target, true)
+    Directory.CreateDirectory target |> ignore
+    for fileInfo in DirectoryInfo(source).EnumerateFiles() do
+        fileInfo.CopyTo(target ++ fileInfo.Name) |> ignore
+
   for sub in [ "."  ] do
     Literate.ProcessDirectory
       ( sources ++ sub, template, output ++ sub, 

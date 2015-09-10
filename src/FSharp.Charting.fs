18349dce80d06beea653d21dd602cccea56fe0cd
Bohdan Szymanik <bohdan.szymanik@gmail.com>
2015-09-10 14:54:59 +1200

    Histogram parameters documentation was incorrect

--- src/FSharp.Charting.fs
+++ src/FSharp.Charting.fs
@@ -2556,20 +2556,22 @@ namespace FSharp.Charting
         /// <param name="data">The data for the chart.</param>
         /// <param name="Name">The name of the data set.</param>
         /// <param name="Title">The title of the chart.</param>
-        /// <param name="Labels">The labels that match the data.</param>
         /// <param name="Color">The color for the data.</param>
         /// <param name="XTitle">The title of the X-axis.</param>
         /// <param name="YTitle">The title of the Y-axis.</param>
+        /// <param name="LowerBound">The lower bound of the histogram.</param>
+        /// <param name="UpperBound">The upper bound of the histogram.</param>
+        /// <param name="Intervals">The number of intervals in the histogram.</param>
         static member Histogram(data:seq<#value>,?Name,?Title,?Color,?XTitle,?YTitle, ?LowerBound, ?UpperBound, ?Intervals) = 
             let data' = data |> Seq.map valueToDouble
             let lowerBound = match LowerBound with
-                            | Some LowerBound -> LowerBound
+                            | Some lowerBound -> lowerBound
                             | _ -> Seq.min data
             let upperBound = match UpperBound with
-                            | Some UpperBound -> UpperBound
+                            | Some upperBound -> upperBound
                             | _ -> Seq.max data
             let intervals = match Intervals with
-                            | Some Intervals -> Intervals
+                            | Some intervals -> intervals
                             | _ -> 30. // corresponds to what ggplot does
             let data'' = (binData data' lowerBound upperBound intervals) |> Seq.map (fun b -> b.Count)
             let labels = (binData data' lowerBound upperBound intervals) |> Seq.map (fun b -> b.LowerBound.ToString())

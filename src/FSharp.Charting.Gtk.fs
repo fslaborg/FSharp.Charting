bbfd8846e1bc75c7eba91fb223e273a34296d0a1
Bohdan Szymanik <bohdan.szymanik@gmail.com>
2015-09-11 16:33:52 +1200

    Format bin labels
    Just need to get them to display!

--- src/FSharp.Charting.Gtk.fs
+++ src/FSharp.Charting.Gtk.fs
@@ -973,7 +973,7 @@ namespace FSharp.Charting
                             | Some intervals -> intervals
                             | _ -> 30. // corresponds to what ggplot does
             let bins = binData data' lowerBound upperBound intervals
-            let data'' = bins |> Seq.map (fun b -> (b.LowerBound.ToString(), b.Count))
+            let data'' = bins |> Seq.map (fun b -> ( sprintf "%.2f" b.LowerBound), b.Count)
             Chart.Column(data'',?Name=Name,?Title=Title,?Color=Color,?XTitle=XTitle,?YTitle=YTitle, ?ColumnWidth=Some 0.95)
 
 #if INCOMPLETE_API
